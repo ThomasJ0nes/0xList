08a3863c28cf698e9ca11b6cb5ba4d6727577885
Florian Hass <f91hama@gmail.com>
2024-08-07 15:14:45 +0000

    Add magnifying glass icon and search button

--- packages/nextjs/app/listing-page/page.tsx
+++ packages/nextjs/app/listing-page/page.tsx
@@ -26,7 +26,15 @@ const PropertyForm: React.FC = () => {
             <p>$50 Per Night</p>
             <p className="text-gray-500">Email: example@example.com</p>
           </div>
-
+          {/* EAS-Search */}
+          <div className="flex justify-end mt-auto">
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+          </button>
+          </div>
+          
           {/* Mint to Contact the Owner Button */}
           <div className="flex justify-end mt-auto">
             <button className="btn btn-primary px-6 py-3 text-lg font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500">
@@ -56,6 +64,14 @@ const PropertyForm: React.FC = () => {
             <p className="text-gray-500">Email: example@example.com</p>
           </div>
 
+          {/* EAS-Search */}
+          <div className="flex justify-end mt-auto">
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+          </button>
+          </div>
           {/* Mint to Contact the Owner Button */}
           <div className="flex justify-end mt-auto">
             <button className="btn btn-primary px-6 py-3 text-lg font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500">
