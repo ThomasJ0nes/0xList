75fe9c751a452df354585fb0245e45df84dbaf40
Tom Jones <94535525+ThomasJ0nes@users.noreply.github.com>
2024-08-11 14:16:08 +0100

    Update meta data

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -5,8 +5,8 @@ import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
-  title: "Scaffold-ETH 2 App",
-  description: "Built with 🏗 Scaffold-ETH 2",
+  title: "0xList",
+  description: "The first Layer 2 on-chain listing marketplace. Buy, sell, or rent anything securely.",
 });
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
